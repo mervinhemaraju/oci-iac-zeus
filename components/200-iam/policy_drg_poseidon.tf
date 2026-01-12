01bbfcabd5b7279592488cb26d8679bd317d34ab
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-01-12 22:45:02 +0400

    - Fixed iam policy for drg poseidon

--- components/200-iam/policy_drg_poseidon.tf
+++ components/200-iam/policy_drg_poseidon.tf
@@ -1,8 +1,10 @@
+
+# Create a policy for DRG management as a requestor
 resource "oci_identity_policy" "poseidon_cross_conection_statements" {
 
   compartment_id = local.values.compartments.root
   name           = "poseidon-cross-connection-statements"
-  description    = "Allow the requestor to manage DRG and RPC in this tenancy and POSEIDON tenancy."
+  description    = "Allow the requestor to manage DRG and RPC in this tenancy and account POSEIDON tenancy."
 
   statements = [
     # Definitions
@@ -10,18 +12,16 @@ resource "oci_identity_policy" "poseidon_cross_conection_statements" {
     "define group ${local.values.groups.poseidon_groups.vcn_admins.name} as ${local.values.groups.poseidon_groups.vcn_admins.id}",
     "define tenancy poseidonTenancy as ${local.values.compartments.root_poseidon}",
 
-    # Allow - ZEUS manages its own resources
-    "allow group ${oci_identity_group.drg_admins.name} to manage virtual-network-family in tenancy",
-    "allow group ${oci_identity_group.drg_admins.name} to manage drgs in tenancy",
-    "allow group ${oci_identity_group.drg_admins.name} to manage remote-peering-from in tenancy",
-
-    # Endorse - ZEUS endorses itself to work with POSEIDON
+    # Allow
+    "Allow group ${oci_identity_group.drg_admins.name} to manage virtual-network-family in tenancy",
+    "Allow group ${local.values.groups.poseidon_groups.vcn_admins.name} to manage virtual-network-family in tenancy",
+    "Allow group ${oci_identity_group.drg_admins.name} to manage remote-peering-from in tenancy",
+    "Allow group ${oci_identity_group.drg_admins.name} to manage drgs in tenancy",
+    # Endorse
     "endorse group ${oci_identity_group.drg_admins.name} to manage drg-attachment in tenancy poseidonTenancy",
-    "endorse group ${oci_identity_group.drg_admins.name} to manage remote-peering-to in tenancy poseidonTenancy",
-
-    # Admit - Accept POSEIDON group
+    "Endorse group ${oci_identity_group.drg_admins.name} to manage remote-peering-to in tenancy poseidonTenancy",
+    # Admit
     "admit group ${local.values.groups.poseidon_groups.vcn_admins.name} of tenancy poseidonTenancy to manage drg in tenancy",
-    "admit group ${local.values.groups.poseidon_groups.vcn_admins.name} of tenancy poseidonTenancy to manage virtual-network-family in tenancy"
   ]
 
   freeform_tags = local.tags.defaults
