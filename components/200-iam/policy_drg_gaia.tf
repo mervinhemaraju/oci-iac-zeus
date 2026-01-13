9d6ca94efa9126660089877be39bd46c1fc7b1ce
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-01-13 11:50:17 +0400

    - Fixed policy issue

--- components/200-iam/policy_drg_gaia.tf
+++ components/200-iam/policy_drg_gaia.tf
@@ -7,27 +7,24 @@ resource "oci_identity_policy" "gaia_database_drg_statements" {
   description    = "Allows oci gaia (requestor) to manage VCN, RPC and DRG attachments in this tenancy."
 
   statements = [
-    # Definition
+    # Definition - MUST be first
     "define group ${oci_identity_group.vcn_admins.name} as ${oci_identity_group.vcn_admins.id}",
     "define group ${local.values.groups.gaia_groups.drg_admins.name} as ${local.values.groups.gaia_groups.drg_admins.id}",
     "define tenancy gaiaTenancy as ${local.values.compartments.root_gaia}",
 
-    # Endorse
+    # Allow (local permissions in Zeus)
+    "allow group ${oci_identity_group.vcn_admins.name} to manage local-peering-gateways in tenancy",
+    "allow group ${oci_identity_group.vcn_admins.name} to manage local-peering-from in tenancy",
+
+    # Endorse (Zeus endorsing its groups to do things in Gaia)
     "endorse group ${local.values.groups.gaia_groups.drg_admins.name} to manage virtual-network-family in tenancy gaiaTenancy",
     "endorse group ${oci_identity_group.vcn_admins.name} to manage virtual-network-family in tenancy gaiaTenancy",
-    "endorse group ${oci_identity_group.vcn_admins.name} to manage local-peering-to in tenancy gaiaTenancy",
-    "endorse group ${oci_identity_group.vcn_admins.name} to associate local-peering-gateways in tenancy gaiaTenancy",
     "endorse group ${oci_identity_group.vcn_admins.name} to manage drg in tenancy gaiaTenancy",
     "endorse group ${oci_identity_group.vcn_admins.name} to manage local-peering-to in tenancy gaiaTenancy",
     "endorse group ${oci_identity_group.vcn_admins.name} to associate local-peering-gateways in tenancy with local-peering-gateways in tenancy gaiaTenancy",
 
-    # Admit
-    "admit group ${local.values.groups.gaia_groups.drg_admins.name} of tenancy gaiaTenancy to manage drg-attachment in tenancy",
-
-    # Allow
-    "allow group ${oci_identity_group.vcn_admins.name} to manage local-peering-gateways in tenancy",
-    "allow group ${oci_identity_group.vcn_admins.name} to manage local-peering-from in tenancy",
-
+    # Admit (allowing Gaia's groups to do things in Zeus)
+    "admit group ${local.values.groups.gaia_groups.drg_admins.name} of tenancy gaiaTenancy to manage drg-attachment in tenancy"
   ]
 
   freeform_tags = local.tags.defaults
