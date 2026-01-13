2bd6a2bb4f47e877dc752a5549ba900f2bfe8e72
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-01-13 10:52:10 +0400

    - Updated iam policies

--- components/200-iam/policy_drg_gaia.tf
+++ components/200-iam/policy_drg_gaia.tf
@@ -16,11 +16,11 @@ resource "oci_identity_policy" "gaia_database_drg_statements" {
     "endorse group ${local.values.groups.gaia_groups.drg_admins.name} to manage virtual-network-family in tenancy gaiaTenancy",
     "endorse group ${oci_identity_group.vcn_admins.name} to manage virtual-network-family in tenancy gaiaTenancy",
     "endorse group ${oci_identity_group.vcn_admins.name} to manage local-peering-to in tenancy gaiaTenancy",
+    "endorse group ${oci_identity_group.vcn_admins.name} to associate local-peering-gateways in tenancy gaiaTenancy",
+    "endorse group ${oci_identity_group.vcn_admins.name} to manage drg in tenancy gaiaTenancy",
 
     # Admit
     "admit group ${local.values.groups.gaia_groups.drg_admins.name} of tenancy gaiaTenancy to manage drg-attachment in tenancy",
-    # Endorse
-    "endorse group ${oci_identity_group.vcn_admins.name} to manage drg in tenancy gaiaTenancy",
 
     # Allow
     "allow group ${oci_identity_group.vcn_admins.name} to manage local-peering-gateways in tenancy"
